9c9564c1de6cb0eb89af5b602748c32c0ba17c67
Emelie Barrington <emelie.barrington@chasacademy.se>
2023-06-16 14:13:18 +0200

    Added gitignore and some local changes

--- js/cart.js
+++ js/cart.js
@@ -7,32 +7,53 @@ let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 //Använd localstorage för att komma åt vad som klickats på  
 
-const generateCartItems = () => {
+const generateCartItems = (item) => {
     // Generera alla produkter med dynamisk HTML och Array.protype.map() samt join()
     //
     // Använd denna markup för varje produktkort - den korresponderar mot CSS:en
     //
-    // <div class="cart-item">
-    // <img width="100" src={--image--} alt="" />
-    // <div class="details">
-    //     <div class="title-price-x">
-    //     <h4 class="title-price">
-    //         <p>{--title--}</p>
-    //         <p class="cart-item-price"> {--price--}</p>
-    //     </h4>
-    //     <i onclick="removeItem({--id--})" class="bi bi-x-lg"></i>
-    //     </div>
-    //     <div class="cart-buttons">
-    //     <div class="buttons">
-    //         <i onclick="decrement({--id--})" class="bi bi-dash-lg"></i>
-    //         <div id={--id--} class="quantity">{--total--}</div>
-    //         <i onclick="increment({--id--})" class="bi bi-plus-lg"></i>
-    //     </div>
-    //     </div>
-    //     <h3> {--total * price--}</h3>
-    // </div>
-    // </div>
- 
+
+    //Steg 1 - utgå ifrån vilka saker som ligger i LocalStorage
+    //Steg 2 - hitta motsvarande produkt i shopData
+    //Steg 3 - Generera de produkter som har samma id 
+
+    if (basket.length != 0) {
+        let indices = []; 
+        basket.forEach(item => {
+            const i = shopData.findIndex(product => product.id === item.productId); 
+            indices.push(i); 
+        });
+        console.log(indices); 
+    }
+    else {
+        console.log("No items in basket"); 
+    }
+
+}
+
+function createItem(item) {
+    return `
+    <div class="cart-item">
+    <img width="100" src=${item.image} alt="store item" />
+    <div class="details">
+        <div class="title-price-x">
+        <h4 class="title-price">
+            <p>${item.title}</p>
+            <p class="cart-item-price"> ${item.price}</p>
+        </h4>
+        <i onclick="removeItem(${item.id})" class="bi bi-x-lg"></i>
+        </div>
+        <div class="cart-buttons">
+        <div class="buttons">
+            <i onclick="decrement(${item.id})" class="bi bi-dash-lg"></i>
+            <div id=${item.id} class="quantity">{--total--}</div>
+            <i onclick="increment({--id--})" class="bi bi-plus-lg"></i>
+        </div>
+        </div>
+        <h3> {--total * price--}</h3>
+    </div>
+    </div>
+    `; 
 }
 
 generateCartItems();
